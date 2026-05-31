92170581db2d88bc338add51bdbcb3232a4c5c8e
Keshav Nalam <nknnkumar@yahoo.com>
2026-05-31 21:04:41 +0530

    v2 start

--- bin/v2/tasks/actions/PostMethods/Alter/steps/locateSource.js
+++ bin/v2/tasks/actions/PostMethods/Alter/steps/locateSource.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -5,9 +6,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const locateSource = () => {
-    return path.join(
-        __dirname,
-        "..",
-        "template"
+    const templatePath = path.join(__dirname, "..", "template");
+
+    const versions = fs.readdirSync(templatePath);
+
+    const maxVersion = Math.max(
+        ...versions.map(v => Number(v.replace("v", "")))
     );
+
+    return path.join(templatePath, `v${maxVersion}`);
 };
